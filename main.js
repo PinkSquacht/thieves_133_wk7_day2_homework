e698877576e2669937a801ee3103d618bc8a8525
PinkSquacht <77953435+PinkSquacht@users.noreply.github.com>
2023-11-29 16:27:44 -0800

    final Commit

--- main.js
+++ main.js
@@ -99,3 +99,69 @@ const getErgastData = async () => {
   };
   
   getErgastData();
+
+// Codewars 
+// Pig latin 
+// Move the first letter of each word to the end of it, 
+// then add "ay" to the end of the word. Leave punctuation marks untouched.
+function pigIt(str){
+    return str.split(' ').map(word => {
+        if (word.match(/^[A-Za-z]+$/)) {
+            return word.slice(1) + word.charAt(0) + 'ay';
+        } else {
+            return word;
+        }
+    }).join(' ');
+}
+
+console.log(pigIt('Pig latin is cool')); // igPay atinlay siay oolcay
+console.log(pigIt('Hello world !'));     // elloHay orldway !
+
+  // test results
+//   const Test = require('@codewars/test-compat');
+
+// describe("Tests", () => {
+//   it("test", () => {
+// Test.assertEquals(pigIt('Pig latin is cool'),'igPay atinlay siay oolcay')
+// Test.assertEquals(pigIt('This is my string'),'hisTay siay ymay tringsay')
+// });
+// });
+
+// Multiples of 3 and 5
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+// Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+
+// Additionally, if the number is negative, return 0.
+
+// Note: If the number is a multiple of both 3 and 5, only count it once
+
+function solution(number){
+    let sum = 0;
+    if (number < 0) {
+        return 0;
+    }
+    for(let i = 0; i < number; i++){
+        if(i % 3 === 0 || i % 5 === 0){
+            sum += i;
+        }
+    }
+    return sum;
+}
+
+console.log(solution(10)); // 23
+
+
+// test results 
+// const { assert } = require("chai")
+
+// function test(n, expected) {
+//   it(`n=${n}`, () => {  
+//     let actual = solution(n)
+//     assert.strictEqual(actual, expected)
+//   })
+// }
+
+// describe("basic tests", function(){
+//   test(10,23)
+// })
